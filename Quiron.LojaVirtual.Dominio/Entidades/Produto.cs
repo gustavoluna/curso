fa7b0398029e7444fe81c68032be4b757ea38362
gustavoluna <luna.gustavo@gmail.com>
2016-11-03 10:12:31 -0200

    entiity

---
+++ Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Entidades
+{
+    public class Produto
+    {
+        public int ProdutoID { get; set; }
+        public String Nome { get; set; }
+        public String Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string Categoria { get; set; }
+    }
+}
