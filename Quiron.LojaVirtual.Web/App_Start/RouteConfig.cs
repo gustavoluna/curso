2071828e7877d99cc82dfad7b6d46ad0419f04f6
gustavoluna <luna.gustavo@gmail.com>
2016-11-03 18:31:18 -0200

    vitrine

--- Quiron.LojaVirtual.Web/App_Start/RouteConfig.cs
+++ Quiron.LojaVirtual.Web/App_Start/RouteConfig.cs
@@ -17,7 +17,7 @@ namespace Quiron.LojaVirtual.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Vitrine", action = "ListaProdutos", id = UrlParameter.Optional }
             );
         }
     }
