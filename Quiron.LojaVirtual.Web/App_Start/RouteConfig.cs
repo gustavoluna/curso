0561e808f23c2d3b40f43995e5f38124595b538d
gustavoluna <luna.gustavo@gmail.com>
2016-11-03 17:54:55 -0200

    g

--- Quiron.LojaVirtual.Web/App_Start/RouteConfig.cs
+++ Quiron.LojaVirtual.Web/App_Start/RouteConfig.cs
@@ -14,8 +14,6 @@ namespace Quiron.LojaVirtual.Web
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-
-
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
